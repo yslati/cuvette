38c4ca479bda6537c97e01959b6c1fb3595946b1
yslati <kaliyassin01@gmail.com>
2024-11-03 12:40:00 +0100

    update backend baseURL

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://cuvetter-api.vercel.app',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://cuvetter-api.onrender.com',
   timeout: 10000,
 })
 
